3db5468f57c786e3b81c33efaea6beab5fcf25c9
Netcafe96 <myblack532@gmail.com>
2025-06-06 12:16:07 +0700

    Update cutter.py

--- utils/cutter.py
+++ utils/cutter.py
@@ -1,4 +1,3 @@
-
 from moviepy.editor import VideoFileClip
 import os
 
@@ -11,5 +10,5 @@ def cut_clips(video_path, matches, output_dir="output_clips"):
         out_path = os.path.join(output_dir, f"clip_{i+1}.mp4")
         clip.write_videofile(out_path, codec="libx264", audio_codec="aac", verbose=False, logger=None)
         clip_paths.append((m["script"], out_path))
-    
+
     return clip_paths
