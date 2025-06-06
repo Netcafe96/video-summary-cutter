48f7dcce4f5b50d03aeb90dee49363486eb20f1b
Netcafe96 <myblack532@gmail.com>
2025-06-06 12:13:33 +0700

    Update transcript.py

--- utils/transcript.py
+++ utils/transcript.py
@@ -1,1 +1,17 @@
+from faster_whisper import WhisperModel
 
+def transcribe_video(video_path):
+    model = WhisperModel("base", compute_type="int8")
+    segments, _ = model.transcribe(video_path)
+    
+    full_text = []
+    segment_list = []
+    for seg in segments:
+        full_text.append(seg.text)
+        segment_list.append({
+            "start": seg.start,
+            "end": seg.end,
+            "text": seg.text
+        })
+    
+    return " ".join(full_text), segment_list
